883a945136439cbc2864eefa01950dd05478820d
JL020928 <jerry020928@gmail.com>
2023-11-08 14:15:14 +0800

    建構隨機資料

--- database/seeders/AnimationsTableSeeder.php
+++ database/seeders/AnimationsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class AnimationsTableSeeder extends Seeder
 {
@@ -12,18 +13,129 @@ class AnimationsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
-        DB::table('animations')->insert([
-            'name'=>'咒術迴戰',
-            'type'=>'冒險、黑暗、奇幻',
-            'orign'=>'芥見下下',
-            'dir'=>'朴性厚',
-            'ep_num'=>24,
-            'cp_id'=>1,
-            'play_time'=>'2020/10/2'
+    
+        public function generateRandomName(){
+            $name = array(
+                '鋼之鍊金術師',
+                '進擊的巨人', 
+                '妖怪少爺',
+                '火影忍者',
+                '海賊王',
+                'BLEACH',
+                '死亡筆記本',  
+                '龍珠',
+                '偶像大師',
+                '命運石之門',
+                'CLANNAD',
+                '鬼滅之刃',
+                '名偵探柯南',  
+                '魔卡少女樂',
+                '魔神Z',
+                '魔法少女小圓',
+                'MIX',
+                '吹響吧!上低音號',
+                '五等分的新娘'  
+                );
+            return $name[rand(0, count($name)-1)];}
 
-        ]);
+        public function generateRandomType(){
+            $type = array(
+                '機戰',
+                '魔法少女',
+                '偶像',
+                '校園',
+                '治癒',
+                '日常',
+                '競技',  
+                '冒險',
+                '奇幻',
+                '科幻',
+                '推理',
+                '恐怖',
+                '戀愛',
+                '搞笑',
+                '戰鬥',
+                '少年',
+                '少女',
+                '魔法',
+                '動作',
+                '懸疑'
+            );
+            return $type[rand(0, count($type)-1)];}
+
+        public function generateRandomOrign(){
+            $orign = array(
+                '岸本斉史',
+                '鳥山明',
+                '高橋留美子',
+                ' CLAMP',
+                '尾田栄一郎',
+                '武內直子',
+                ' relator',
+                '松本大洋',
+                '荒川弘',
+                '石原恭央' ,
+                '田尻智',
+                '井上雄彥',
+                '高殿円',
+                '藤子不二雄',
+                '古味直志',
+                '桂正和',
+                '五十嵐正邦',
+                '花塚明' ,
+                '赤松健',
+                '横山光輝'
+                );
+            return $orign[rand(0, count($orign)-1)];}
+
+        public function generateRandomDir(){
+            $dir = array(
+                '宮崎駿',
+                '今石洋之',
+                '庵野秀明',  
+                '押井守',
+                '細田守',
+                '大友克洋',
+                '德間康快',
+                '萩原一至',
+                '樋口真嗣',
+                '新房昭之',
+                '川尻善昭',
+                '村瀬繪里子',
+                '山賀博之',  
+                '糸井重里',
+                '神山健治',
+                '摩砂雪',
+                '大張正己',
+                '富野由悠季',
+                '結城信輝',
+                '高畑勲'
+                );
+            return $dir[rand(0, count($dir)-1)];}
+    
+     public function run()
+    {
+        for ($i=0; $i<20; $i++)   // 隨機生成數
+        {
+            $name = $this->generateRandomName();
+            $type = $this->generateRandomType();
+            $orign = $this->generateRandomOrign();
+            $dir = $this->generateRandomDir();
+            $play_time = Carbon::now()->subYears(rand(10, 20))->subMonths(rand(0, 12))->subRealDays(rand(0, 31));
+            $created_at = Carbon::now();
+            $updated_at = Carbon::now();
+            DB::table('animations')->insert([
+                'name'=>$name,
+                'type'=>$type,
+                'orign'=>$orign,
+                'dir'=>$dir,
+                'ep_num'=>rand(6, 24),
+                'cp_id'=>rand(1, 12),
+                'play_time'=>$play_time,
+                'created_at'=>$created_at,
+                'updated_at'=>$updated_at
+
+            ]);
+        }
     }
 }
--- database/seeders/CompaniesTableSeeder.php
+++ database/seeders/CompaniesTableSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,15 +13,129 @@ class CompaniesTableSeeder extends Seeder
      *
      * @return void
      */
+
+    public function generateRandomFounder(){
+        $founder = array(
+            '宮崎駿', // Studio Ghibli
+            '庵野秀明', // Gainax
+            '今石洋之', // Madhouse
+            '德間康快', // Kyoto Animation
+            '橘正紀', // P.A. Works
+            '岡田斗司夫', // Studio Deen 
+            '東憲一郎', //Production I.G
+            '石川光久', // Bones
+            '近藤光', // Sunrise 
+            '森下隆司', // A-1 Pictures
+            '丸山正雄', // J.C.Staff
+            '內藤泰弘', // ufotable
+            '新房昭之', // Shaft
+            '佐藤順一', //OLM
+            '片山一良', // Artland
+            '岡本幸代', // Tatsunoko
+            '大隅正秋', // Trigger  
+            '福田己津央', // feel
+            '劉維', // Haoliners
+            '能海寬', // Xebec
+            '川瀬健太郎', // LIDEN FILMS
+        );
+        return $founder[rand(0, count($founder)-1)];}
+    
+    public function generateRandomName(){
+        $name = array(
+            '京都動畫',
+            'MAPPA',
+            'Production I.G',
+            'A-1 Pictures',
+            'Studio Ghibli',
+            'GAINAX',
+            'Sunrise',
+            'BONES',
+            'P.A. Works',
+            'Madhouse',
+            'J.C.Staff',
+            'ufotable',
+            'SHAFT',
+            '細田',
+            'Production IMS',
+            'GANSIS',
+            'Trigger',
+            '白絲',
+            'XEBEC',
+            'OLM'
+            );
+        return $name[rand(0, count($name)-1)];}
+
+    public function generateRandomHead(){
+        $head = array(
+            '東京都西東京市', //Studio Ghibli
+            '京都府宇治市', // Kyoto Animation
+            '東京都新宿區', // Production I.G
+            '東京都千代田區', // Madhouse
+            '東京都墨田區', // A-1 Pictures 
+            '東京都中野區', // Sunrise
+            '東京都台東區', // Bones  
+            '神奈川縣橫濱市', // P.A Works
+            '東京都武蔵野市', // ufotable
+            '東京都澀谷區', // SHAFT
+            '東京都調布市', // J.C.Staff
+            '東京都立川市', // feel
+            '青森縣八戶市', //OLM 
+            '東京都千代田區', // MAPPA
+            '大阪府吹田市', // Gainax
+            '東京都中野區', // Trigger
+            '東京都墨田區', // Haoliners
+            '東京都新宿區', // LIDEN FILMS  
+            '千葉縣浦安市', // XEBEC
+            '東京都武蔵野市', // TMS Entertainment
+        );
+            return $head[rand(0, count($head)-1)];}
+
+    public function generateRandomAddress(){
+        $address = array(
+            'https://www.toei-anim.co.jp/', // Toei Animation
+            'https://www.tatsunoko.co.jp/', // Tatsunoko
+            'https://www.kyotoanimation.co.jp/', // Kyoto Animation
+            'http://www.a1pictures.com/', // A-1 Pictures
+            'https://www.igport.co.jp/', // Production I.G
+            'https://www.madhouse.co.jp/', // Madhouse
+            'http://www.sunrise-inc.co.jp/', // Sunrise
+            'http://www.bones.co.jp/', // Bones
+            'https://www.khara.co.jp/', // Studio Khara
+            'https://www.paworks.com/', // P.A. Works
+            'http://www.ufotable.com/', // ufotable
+            'http://www.jcstaff.co.jp/', // J.C.Staff
+            'http://www.gainax.co.jp/', // Gainax 
+            'http://www.tms-e.com/', // TMS Entertainment
+            'http://www.animatefilm.com/', // Animate Film
+            'https://www.olm.co.jp/', // OLM
+            'http://www.p-productions.co.jp/', // P Productions
+            'https://trigger.co.jp/', // Trigger
+            'https://www.artland.co.jp/', // Artland
+            'http://www.silverlink.co.jp/', // Silver Link  
+        );
+            return $address[rand(0, count($address)-1)];}
+        
+
     public function run()
     {
-        //
-        DB::table('companies')->insert([
-            'name'=>'MAPPA',
-            'founder'=>'丸山正雄',
-            'head'=>'日本東京都杉並區天沼2丁目3番9號朝日生命杉並大樓9樓',
-            'address'=>'https://www.mappa.co.jp/en/'
-
-        ]);
+        for ($i=0; $i<10; $i++)   // 隨機生成數
+        {
+            $name = $this->generateRandomName();
+            $founder = $this->generateRandomFounder();
+            $head = $this->generateRandomHead();
+            $address = $this->generateRandomAddress();
+            $create = Carbon::now()->subYears(rand(15, 25))->subMonths(rand(0, 12))->subRealDays(rand(0, 31));
+            $created_at = Carbon::now();
+            $updated_at = Carbon::now();
+            DB::table('companies')->insert([
+                'name'=>$name,
+                'create'=>$create,
+                'founder'=>$founder,
+                'head'=>$head,
+                'address'=>$address,
+                'created_at'=>$created_at,
+                'updated_at'=>$updated_at
+            ]);
+        }
     }
 }
