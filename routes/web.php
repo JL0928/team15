aad392518e72725e6fe8d7bde2bb8e3fd8886e5d
JL020928 <jerry020928@gmail.com>
2023-12-06 14:13:13 +0800

    增加route註解

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,21 @@ use App\Http\Controllers\CompaniesController;
 Route::get('/', function(){
     return redirect('animations');
 });
-
+//顯示所有Animation資料
 Route::get('animations',[AnimationsController::class,'index'])->name('animations.index');
+//顯示單一Animation資料
 Route::get('animations/{id}',[AnimationsController::class,'show'])->where('id','[0-9]+')->name('animations.show');
+//修改單一Animation資料
 Route::get('animations/{id}/edit',[AnimationsController::class,'edit'])->where('id','[0-9]+')->name('animations.edit');
+//刪除單一Animation資料
 Route::delete('animations/delete/{id}',[AnimationsController::class,'destroy'])->where('id','[0-9]+')->name('animations.destroy');
 
 
+//顯示所有Company資料
 Route::get('companies',[CompaniesController::class,'index'])->name('companies.index');
+//顯示單一Company資料
 Route::get('companies/{id}',[CompaniesController::class,'show'])->where('id','[0-9]+')->name('companies.show');
+//修改單一Company資料
 Route::get('companies/{id}/edit',[CompaniesController::class,'edit'])->where('id','[0-9]+')->name('companies.edit');Route::get('animations/{id}/edit',[AnimationsController::class,'edit'])->where('id','[0-9]+')->name('animations.edit');
+//刪除單一Company資料
 Route::delete('companies/delete/{id}',[CompaniesController::class,'destroy'])->where('id','[0-9]+')->name('companies.destroy');
