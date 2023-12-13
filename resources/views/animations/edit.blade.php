93e8a75770805e3afd5a964679ffa29e4d89885a
D1104183002 <D1104183002@gm.lhu.edu.tw>
2023-12-13 13:40:15 +0800

    設計新增動畫的表單

---
+++ resources/views/animations/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('title', '編輯特定動畫')
+
+@section('animations_theme', '編輯中的動畫')
+
+@section('animations_contents')
+    {!! Form::model($animation, ['method'=>'PATCH', 'action'=>['\App\Http\Controllers\AnimationsController@update', $animation->id]]) !!}
+    @include('animations.form', ['submitButtonText'=>"更新動畫資料"])
+    {!! Form::close() !!}
+@endsection
