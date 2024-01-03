a339b736c1a38843cbe0a4aa192f09e94fc9af3f
sung911021 <sung911021@gmail.com>
2024-01-03 21:08:59 +0800

    下載安裝Node.js、新增下拉式選單、類別查詢功能及功能修正(權限未設置)

--- routes/web.php
+++ routes/web.php
@@ -66,3 +66,7 @@ Route::middleware(['auth'])->group(function(){
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
